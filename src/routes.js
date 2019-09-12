2dd29bc16d0635923ffb4f210efc0f4a64fcf7a1
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-09-12 08:37:10 -0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Home from "./components/Home.vue";
+import Discord from "./components/Discord.vue";
 
 const routes = [
-  { path: "/", component: Home }
+  { path: "/", component: Home },
+  { path: "/discord", component: Discord}
 ];
 
 export default routes;
