82ca09792b6332cfef3fa7fff99f62a1fd59f975
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-09-12 08:36:29 -0500

    Create Discord.vue

---
+++ src/components/Discord.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="browsers">
+    <ul class="list">
+      <span class="title">Discord</span>
+      <li>
+        <a>
+          <img src="https://img.shields.io/discord/451069918474141706.svg?label=discord" class="shadow">
+        </a>
+      </li>
+      <li v-for="item in items">
+        <a :href="''+item.url">
+          <img :src="'https://img.shields.io/badge/-' + item.name + '-' + item.color +'.svg'" :class="'shadow'">
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Discord",
+  metaInfo: {
+    title: "Discord"
+  },
+  data: function() {
+    return {
+      items: [
+        {
+          name: "Join",
+          color: "green",
+          url: "https://invite.gg/kermit"
+        }
+      ]
+    };
+  }
+};
+</script>
